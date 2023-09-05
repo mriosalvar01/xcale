5a90fcf6519bbfcc39a88f571e4095fc963a536a
Manuel Rios <manuel.rios_consultor@mdp.com.pe>
2023-09-05 10:44:11 -0500

    feat: add structure project

---
+++ src/main/java/com/example/xcale/XcaleApplication.java
@@ -0,0 +1,13 @@
+package com.example.xcale;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class XcaleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(XcaleApplication.class, args);
+	}
+
+}
